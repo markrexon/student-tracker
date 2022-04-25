b9cb2ae9d6a7693ad77f2c8f2572ae0592007eda
markrexon <markrexon1999@gmail.com>
2022-04-25 21:29:03 +0530

    commit1

---
+++ adminsession/views.py
@@ -0,0 +1,74 @@
+import json
+import requests
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.views.decorators.csrf import csrf_exempt
+
+from .EmailBackend import EmailBackend
+from .models import Attendance, Session, Subject
+
+# Create your views here.
+
+
+def login_page(request):
+    if request.user.is_authenticated:
+        if request.user.user_type == '1':
+            return redirect(reverse("add_staff"))
+        elif request.user.user_type == '2':
+            return redirect(reverse("staff_home"))
+        else:
+            return redirect(reverse("student_view_profile"))
+    return render(request, 'main_app/login.html')
+
+
+def doLogin(request, **kwargs):
+    if request.method != 'POST':
+        return HttpResponse("<h4>Denied</h4>")
+    else:
+        user = EmailBackend.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
+        if user != None:
+            login(request, user)
+            if user.user_type == '1':
+                return redirect(reverse("add_staff"))
+            elif user.user_type == '2':
+                return redirect(reverse("staff_home"))
+            else:
+                return redirect(reverse("student_home"))
+        else:
+            messages.error(request, "Invalid details")
+            return redirect("/")
+
+
+
+def logout_user(request):
+    if request.user != None:
+        logout(request)
+    return redirect("/")
+
+
+@csrf_exempt
+def get_attendance(request):
+    subject_id = request.POST.get('subject')
+    session_id = request.POST.get('session')
+    try:
+        subject = get_object_or_404(Subject, id=subject_id)
+        session = get_object_or_404(Session, id=session_id)
+        attendance = Attendance.objects.filter(subject=subject, session=session)
+        attendance_list = []
+        for attd in attendance:
+            data = {
+                    "id": attd.id,
+                    "attendance_date": str(attd.date),
+                    "session": attd.session.id
+                    }
+            attendance_list.append(data)
+        return JsonResponse(json.dumps(attendance_list), safe=False)
+    except Exception as e:
+        return None
+
+
+def showFirebaseJS(request):
+    data =""
+    return HttpResponse(data, content_type='application/javascript')
